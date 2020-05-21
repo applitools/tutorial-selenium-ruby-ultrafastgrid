44fe50406f83d568c6848308bf38ff9ef2e86d15
artem <tranduil15@gmail.com>
2020-05-21 19:22:36 +0300

    update to the java example update readme

--- simple_test_script.rb
+++ simple_test_script.rb
@@ -1,37 +1,47 @@
 require 'eyes_selenium'
 
-visual_grid_runner = Applitools::Selenium::VisualGridRunner.new(10)
-eyes = Applitools::Selenium::Eyes.new(runner: visual_grid_runner)
-
+# Create a new chrome web driver
 web_driver = Selenium::WebDriver.for :chrome
 
-eyes.batch = Applitools::BatchInfo.new("Ultrafast Batch")
+# Create a runner with concurrency of 1
+visual_grid_runner = Applitools::Selenium::VisualGridRunner.new(1)
 
+# Create Eyes object with the runner, meaning it'll be a Visual Grid eyes.
+eyes = Applitools::Selenium::Eyes.new(runner: visual_grid_runner)
+
+# Initialize eyes Configuration
 eyes.configure do |conf|
+  #  You can get your api key from the Applitools dashboard
+  conf.api_key = 'APPLITOOLS_API_KEY'
+  # create a new batch info instance and set it to the configuration
+  conf.batch = Applitools::BatchInfo.new("Ultrafast Batch")
   conf.app_name = 'Demo App'
   conf.test_name = 'Ultrafast grid demo'
-  # conf.proxy = Applitools::Connectivity::Proxy.new('http://your.proxy.com')
   conf.viewport_size = Applitools::RectangleSize.new(800, 600)
+  # Add browsers with different viewports
   conf.add_browser(800, 600, BrowserTypes::CHROME)
       .add_browser(700, 500, BrowserTypes::FIREFOX)
       .add_browser(800,600, BrowserTypes::SAFARI)
       .add_browser(1600,1200, BrowserTypes::IE_11)
       .add_browser(1024,768, BrowserTypes::EDGE_CHROMIUM)
-      .add_device_emulation(Devices::IPhoneX, Orientations::PORTRAIT)
+  #  Add mobile emulation devices in Portrait mode
+  conf.add_device_emulation(Devices::IPhoneX, Orientations::PORTRAIT)
       .add_device_emulation(Devices::Pixel2, Orientations::PORTRAIT)
 end
 
+ # ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
+ # but then change the above URL to https://demo.applitools.com/index_v2.html
+ # (for the 2nd run)
 begin
+
   # Call Open on eyes to initialize a test session
   driver = eyes.open(driver: web_driver)
 
   # Navigate to the url we want to test
   driver.get('https://demo.applitools.com/index.html')
 
-  # Note to see visual bugs, run the test using the above URL for the 1st run.
-  # but then change the above URL to https://demo.applitools.com/index_v2.html (for the 2nd run)
-
-  # check the login page
+  # check the login page with fluent api, see more info here
+  # https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
   eyes.check('Login page', Applitools::Selenium::Target.window.fully)
 
   # Click the 'Log In' button
@@ -40,6 +50,7 @@ begin
   # Check the app page
   eyes.check('App Page', Applitools::Selenium::Target.window.fully)
 
+  # Call Close on eyes to let the server know it should display the results
   eyes.close_async
 rescue => e
   puts e.message
@@ -48,6 +59,9 @@ rescue => e
 ensure
   # Close the browser
   driver.quit
+
+  # we pass false to this method to suppress the exception that is thrown if we
+  # find visual differences
   results = visual_grid_runner.get_all_test_results
   puts results
 end
